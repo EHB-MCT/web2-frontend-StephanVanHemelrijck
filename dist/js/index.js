b34d9de7988127b850283ba8ce9952be7d0e6b9d
Stephan Van Hemelrijck <stephan.van.hemelrijck@student.ehb.be>
2021-12-15 21:33:19 +0100

    Made basic login + some css changes

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,1 +1,55 @@
-console.log("logging js");
+"use strict";
+
+window.onload = function () {
+    initFields();
+};
+
+function initFields() {
+    document.getElementById("login").addEventListener("click", async (e) => {
+        e.preventDefault();
+
+        let credentials = await checkInput();
+        userLogin(credentials[0], credentials[1]);
+    });
+}
+
+function checkInput() {
+    // Assigning values
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    // Validation input fields
+    let inputs = document.getElementsByClassName("input");
+    if (!email || !password) {
+        let htmlString = `<p id="missing-message">Fill in the form, missing: <br>`;
+        let htmlInputsMissing = "";
+        let htmlStringEnd = ` </p>`;
+        for (let input of inputs) {
+            if (!input.value) {
+                htmlInputsMissing += `${input.name}, `;
+            }
+        }
+        // .slice used to delete the "," for the last element
+        const finalHTML = htmlString + htmlInputsMissing.slice(0, htmlInputsMissing.length - 2) + htmlStringEnd;
+        document.getElementById("missing-message-container").innerHTML = finalHTML;
+        return;
+    }
+    return [email, password];
+}
+
+function userLogin(email, password) {
+    fetch("https://web2-routexploreapi.herokuapp.com/users/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ email: email, password: password }),
+    })
+        .then((res) => res.json())
+        .then((data) => {
+            if (data.error) {
+                document.getElementById("message-container").innerHTML = `<p id="error-message">${data.value} </p>`;
+            } else {
+                document.getElementById("message-container").innerHTML = `<p id="error-message">${data.message} </p>`;
+                window.location.href = "../html/home.html";
+            }
+        });
+}
