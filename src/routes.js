77094048816e2690d11a1cdb81663cbc056c12d3
Stephan Van Hemelrijck <stephan.van.hemelrijck@student.ehb.be>
2021-12-21 01:02:29 +0100

    Able to search routes based on city, and reset search

--- src/routes.js
+++ src/routes.js
@@ -10,7 +10,16 @@ window.onload = async function () {
     personalizeHTML();
     document.getElementById("big-container").innerHTML = `<h1 style="color:#FFFFFF; padding: 50px;">Loading Routes...</h1>`;
     await printRoutes();
+    document.getElementById("search").addEventListener("click", (e) => searchRoutes(e));
+    document.getElementById("delete").addEventListener("click", printRoutes());
 };
+
+function searchRoutes(e) {
+    e.preventDefault();
+    // Assigning search value
+    const city = document.getElementById("searchvalue").value;
+    printRoutesByCity(city);
+}
 
 function personalizeHTML() {
     let user = cookie.getCookie("username");
@@ -20,19 +29,19 @@ function personalizeHTML() {
     document.getElementById("username-nav").innerHTML = userCapitalCase;
 }
 
-async function printRoutes() {
-    await fetch("https://web2-routexploreapi.herokuapp.com/routes", {
-        method: "GET",
-    })
+async function printRoutesByCity(city) {
+    console.log(city);
+    await fetch("https://web2-routexploreapi.herokuapp.com/routes/city/:city" + "?city=" + city)
         .then((res) => res.json())
         .then((data) => {
-            console.log(data);
+            document.getElementById("big-container").innerHTML = "";
             let htmlString = ``;
             data.forEach((element) => {
+                // Clear the loading message
                 // Capitalize username
                 const str = element.created_by;
                 const userName = element.created_by.charAt(0).toUpperCase() + str.slice(1);
-                htmlString += ` <div id="container">
+                htmlString = ` <div id="container">
                 <div id="container-img">
                 <img src="${element.route_img_url}" alt="${element.route_name}" />
                 </div>
@@ -45,7 +54,38 @@ async function printRoutes() {
                 <p id="startpoint"><span>Startpoint:</span> ${element.route_start_location.city}, ${element.route_start_location.state}, ${element.route_start_location.country}</p>
                 </div>
                 </div>`;
+                document.getElementById("big-container").insertAdjacentHTML("afterbegin", htmlString);
             });
-            document.getElementById("big-container").innerHTML = htmlString;
+        });
+}
+
+async function printRoutes() {
+    await fetch("https://web2-routexploreapi.herokuapp.com/routes", {
+        method: "GET",
+    })
+        .then((res) => res.json())
+        .then((data) => {
+            document.getElementById("big-container").innerHTML = "";
+            let htmlString = ``;
+            data.forEach((element) => {
+                // Clear the loading message
+                // Capitalize username
+                const str = element.created_by;
+                const userName = element.created_by.charAt(0).toUpperCase() + str.slice(1);
+                htmlString = ` <div id="container">
+                <div id="container-img">
+                <img src="${element.route_img_url}" alt="${element.route_name}" />
+                </div>
+                <div id="description">
+                <div id="names">
+                <p id="routename"><span>Route:</span> ${element.route_name}</p>
+                <p id="creatorname"><span>Created by:</span> ${userName}</p>
+                <div id="${element.route_id}" class="icon-star-full addtofav"></div>
+                </div>
+                <p id="startpoint"><span>Startpoint:</span> ${element.route_start_location.city}, ${element.route_start_location.state}, ${element.route_start_location.country}</p>
+                </div>
+                </div>`;
+                document.getElementById("big-container").insertAdjacentHTML("afterbegin", htmlString);
+            });
         });
 }
